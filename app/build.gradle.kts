6d9510bb1b6e594596e1a737b5d7a6b71448cb70
Moe <mohamadk@umich.edu>
2021-11-21 22:00:25 -0500

    Removed unused dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,12 +48,9 @@ android.run  {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
     implementation("androidx.activity:activity-compose:1.4.0")
     implementation("com.google.dagger:hilt-android:2.40.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0-beta01")
@@ -65,8 +62,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
     implementation("com.jakewharton.timber:timber:5.0.1")
-    implementation("androidx.paging:paging-runtime:3.1.0")
-    implementation("androidx.paging:paging-compose:1.0.0-alpha14")
     implementation("androidx.room:room-runtime:2.4.0-alpha04")
     implementation("androidx.room:room-ktx:2.4.0-alpha04")
     implementation("io.github.kaustubhpatange:autobindings-room:1.1-beta05")
@@ -76,8 +71,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
-
-    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
 
     kapt("com.google.dagger:hilt-android-compiler:2.40.1")
     kapt("androidx.room:room-compiler:2.4.0-alpha04")
