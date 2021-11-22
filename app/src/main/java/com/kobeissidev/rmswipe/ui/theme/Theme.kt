4c4663b4811a74d5199ccdca61c5b44279d773ee
Moe <mohamadk@umich.edu>
2021-11-21 23:26:13 -0500

    Added screenshots

--- app/src/main/java/com/kobeissidev/rmswipe/ui/theme/Theme.kt
+++ app/src/main/java/com/kobeissidev/rmswipe/ui/theme/Theme.kt
@@ -1,38 +1,21 @@
 package com.kobeissidev.rmswipe.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion
-
-private val DarkColorPalette = darkColors(
-    primary = RMGreen,
-    onPrimary = Color.Black,
-    primaryVariant = RMGreen,
-    secondary = RMGreen,
-    background = RMBlue,
-    surface = RMGreen
-)
-
-private val LightColorPalette = lightColors(
-    primary = RMGreen,
-    onPrimary = Color.Black,
-    primaryVariant = RMGreen,
-    secondary = RMGreen,
-    background = RMBlue,
-    surface = RMGreen
-)
 
 @Composable
-fun RMSwipeTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
+fun RMSwipeTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        colors = if (darkTheme) DarkColorPalette else LightColorPalette,
+        colors = darkColors(
+            primary = RMGreen,
+            onPrimary = Color.Black,
+            primaryVariant = RMGreen,
+            secondary = RMGreen,
+            background = RMBlue,
+            surface = RMGreen
+        ),
         typography = Typography,
         shapes = Shapes,
         content = content
